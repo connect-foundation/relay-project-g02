d36aa1d0b502f927d3c2cfa0562ae830d9af5782
Seung Bin . Ko <kosb15@naver.com>
2019-07-26 16:24:00 +0900

    이미지 파일 업로드 기능 추가

---
+++ server/front/index.js
@@ -0,0 +1,27 @@
+const changeInputFileLabel = () => {
+    const inputFile = document.getElementById('inputFile')
+    inputFile.addEventListener('change', () => {
+        document.getElementById('inputFileLabel').innerText = inputFile.files[0].name;
+    });
+};
+
+const sendInputFileImage = () => {
+    document.getElementById('inputFileForm')
+        .addEventListener('submit', (event) => {
+            const formData = new FormData(event.target);
+            const url = 'http://localhost:8080/upload';
+            const options = {
+                method: 'POST',
+                body: formData,
+                mode: 'no-cors'
+            };
+
+            fetch(url, options);
+
+        });
+};
+
+window.onload = () => {
+    changeInputFileLabel();
+    sendInputFileImage();
+};
