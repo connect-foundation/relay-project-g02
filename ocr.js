8d8ad3862fd637fc17541f70c856861ac1147428
RBJH <46277703+RBJH@users.noreply.github.com>
2019-07-26 15:51:56 +0900

    Create OCR

---
+++ ocr.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  const fileName = './test.jpg';
+
+  // Performs text detection on the local file
+  const [result] = await client.textDetection(fileName);
+  const detections = result.textAnnotations;
+  console.log('Text:');
+  detections.forEach(text => console.log(text));
+}
+
+quickstart();
