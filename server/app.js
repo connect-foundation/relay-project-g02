5226b6a432e2578901daf84cc6d686881c65de02
bbumjun <bumjun7928@naver.com>
2019-07-26 15:19:32 +0900

    파일 경로 설정

--- server/app.js
+++ server/app.js
@@ -3,10 +3,19 @@ let uuid = require('uuid');
 
 let app = express();
 let id = uuid.v4();
-let port = 8080;
+let port = 8090;
 
 let multer = require('multer');
-let upload = multer({ dest: 'uploads/' })
+
+let storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null,'uploads/')
+    },
+    filename: function (req,file,cb) {
+        cb(null,file.originalname)
+    }
+})
+let upload = multer({storage : storage})
 
 app.post('/upload', upload.single('userfile'), function(req, res){
     res.send('Uploaded! : '+req.file); // object를 리턴함
