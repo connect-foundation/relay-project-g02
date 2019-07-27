dfd74d1df1429ba7199066710dabff10e9f5fc89
YoungAndDo <yd199323@gmail.com>
2019-07-28 00:23:24 +0900

    google vision api 테스트 및 결과 front에 출력 가능하도록

--- server/app.js
+++ server/app.js
@@ -4,64 +4,51 @@ const path = require('path');
 
 const app = express();
 const port = 8080;
+// const _projectId = 'boostcamp';
+// const _keyFilename = path.join(__dirname, '../keys/boostcamp-9c749912510c.json');
+const projectId = 'my-ocr-proj';
+const keyFilename = path.join(__dirname, '../keys/my-ocr-proj-3ead5d620204.json');
 
 app.use(express.json());
 
 require("dotenv").config();
 
 async function start(filename){
-  // Imports the Google Cloud client library
   const vision = require('@google-cloud/vision');
-
   // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+  const client = new vision.ImageAnnotatorClient({
+    projectId, keyFilename
+  });
 
   // Performs text detection on the local file
+  console.log(filename);
   const [result] = await client.textDetection(filename);
-  const detections = result.textAnnotations;
-  console.log('Text:');
-  let str = '';
-  detections.forEach(text => {
-      str += `${text.description}`
-  });
-  console.log(str);
-  let fn = filename.slice(0, -4);
+  const detections = result.textAnnotations[0].description;
   // fs.writeFileSync(`./${fn}.txt`, str, "utf8");
-  return new Promise(resolve => resolve({
-    filename: fn,
-    text: str
-  }));
+  return new Promise(resolve => resolve(detections));
 }
 
 const multer = require('multer');
 const storage = multer.diskStorage({
-  destination: function(req, file, callback){
-    callback(null,'uploads/')
+  destination: function(req, file, cb){
+    cb(null, path.join(__dirname, 'uploads'))
   },
-  filename: function (req, file, callback) {
-    callback(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) // cb 콜백함수를 통해 전송된 파일 이름 설정
   }
 })
 const upload = multer({ storage: storage })
 
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'front')));
 
 app.post('/upload', upload.single('userfile'), async (req, res) => {
-  try {
-    console.log(req.file.path);
-    const result = await start(`./${req.file.path}`);
-  
-    console.log(req.file)
-      res.send({ 
-        upload: 'success',
-        filename: req.file
-      }); // object를 리턴함
-      // console.log(req); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
-  } catch (error) {
-    console.log(error);
-  }
- 
+  console.log(req.file);
+  const result = await start(req.file.path);
+  res.status(201).json({
+    result,
+    filepath: '/uploads/'+req.file.filename 
+  });
 });
 
 
--- server/front/index.js
+++ server/front/index.js
@@ -9,18 +9,30 @@ const sendInputFileImage = () => {
     document.getElementById('inputFileForm')
         .addEventListener('submit', (event) => {
             const formData = new FormData(event.target);
+            const ocrResult = document.getElementById('ocrResult');
             const url = 'http://localhost:8080/upload';
             const options = {
                 method: 'POST',
                 body: formData
             };
             fetch(url, options)
-                .then((response) => {
-                    console.log(response);
-                    document.getElementById('ocrResult').innerText = 'wow';
-                });
-            document.getElementById('ocrResult').innerText = 'wow';
-
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    ocrResult.innerText = data.result;
+                    const uploadedImg = document.getElementById('requestImage');
+                    uploadedImg.src = data.filepath;
+                    uploadedImg.style.visibility = 'visible';
+                })
+                .catch(error => {
+                    console.log(error);
+                    ocrResult.innerText = error;
+                })
+                .finally(() => {
+                    ocrResult.classList.remove('blinking');
+                })
+            ocrResult.innerText = '|';
+            ocrResult.classList.add('blinking');
         });
 };
 
