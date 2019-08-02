d9bb059d8df23de159497bd3560dd9ad3c82eae9
KIONLEE <>
2019-08-02 18:13:15 +0900

    api 완료

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-
+​
 async function translate(sourceText) {
     const key = "AIzaSyAQRrXiza4ATn621-HJXem7tKPAGmHnVKc"; //이부분 개인 계정으로 발급받은 KEY값이라 변경해서 사용해 주시기 바랍니다.
     // key 발급은 docs 참고 
@@ -14,9 +14,8 @@ async function translate(sourceText) {
                 target: targetLang,
                 key: key
             }
-        }
-    );
-    return translatedText;
+        });
+    return translatedText.data.data.translations[0].translatedText;
 }
-
+​
 module.exports = translate;
